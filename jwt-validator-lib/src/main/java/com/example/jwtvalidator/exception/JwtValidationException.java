d4edb7fac138f91c3b75e74c5ba49a43fbed5b99
UPPIGIT <42774514+UPPIGIT@users.noreply.github.com>
2025-08-01 15:30:07 +0530

    zipped

--- jwt-validator-lib/src/main/java/com/example/jwtvalidator/exception/JwtValidationException.java
+++
@@ -1,6 +0,0 @@
-package com.example.jwtvalidator.exception;
-
-public class JwtValidationException extends Exception {
-    public JwtValidationException(String message) { super(message); }
-    public JwtValidationException(String message, Throwable cause) { super(message, cause); }
-}
